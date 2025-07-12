efe97c1be113192a9419b87ce48e311819d21ddf
knock5 <miftachulhd5@gmail.com>
2025-07-12 19:27:58 +0700

    add: initialize project

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+document.getElementById("app").innerHTML = `
+  <h1>📦 Stock Bin Card App</h1>
+  <p>Proyek dimulai! Siap dikembangkan.</p>
+`;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
+    port: 8008,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  mode: "development",
+};
